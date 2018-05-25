1468f8355f83ab40e9fbe25335b6ecdeb49b5981
nguyentienanhvtth <nguyentienanhvtth@gmail.com>
2018-05-25 10:53:02 +0700

    up len cho a Duy sua

--- public/js/bundle-debug.js
+++ public/js/bundle-debug.js
@@ -1,46 +1,8 @@
 jQuery(document).ready(function(){
-  var owl = $('.auto-play-slider.owl-carousel');
-	owl.owlCarousel({
-	    items:1,
-	    loop:true,
-	    margin:10,
-	    autoplay:false,
-	    autoplayTimeout:5500,
-	    animateOut: 'fadeOut'
+   $('.grid').masonry({
+	  // options
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+		columnWidth: '.grid-item'
 	});
-	$("#menu-bar").click(function () {
-		// body...
-		$("#clone").find("nav.menu").slideToggle('fast');
-	});
-});
-
-jQuery(document).ready(function(){
-  $(window).on('load', function () {
-		$('.ma5slider').ma5slider();
- });
-
-});
-$(document).ready(function(){
-
-	//Check to see if the window is top if not then display button
-		$(window).scroll(function(){
-			if ($(this).scrollTop() > 400) {
-					$('.button-scroll').fadeIn();
-			} else {
-					$('.button-scroll').fadeOut();
-			}
-	});
-
-	//Click event to scroll to top
-	$('.button-scroll').click(function(){
-			$('html, body').animate({scrollTop : 0},800);
-			return false;
-	});
-						 
-
-});
-jQuery(document).ready(function(){
-  
-	new WOW().init();
-    
 });
